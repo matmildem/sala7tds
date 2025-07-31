414cd60aa992e5470b89e1332d2d80a248644c07
matmildem <matmildem1706@gmail.com>
2025-07-31 11:30:02 -0300

    Add files via upload

--- lading page/script.js
+++ lading page/script.js
@@ -1,17 +1,22 @@
-function enviar (){
-    document.getElementById('nome').style.border = '1px solid black'
-    document.getElementById('assunto').style.border = '1px solid black'
+function enviar(){
+    //volta as bordas para preto normalizando o input
+    document.getElementById('nome').style.border = "1px solid black"
+    document.getElementById('assunto').style.border = "1px solid black"
 
-    const nome=document.getElementById('nome').value
+    //atribui para variaveis constantes os valores digitados pelo usuario
+    const nome = document.getElementById('nome').value 
     const assunto = document.getElementById('assunto').value
+    
+    //condicional para verificar se nao há nome e assunto digitados,
+    //caso nao digitados as bordas do input ficam vermelhas
+    if(!nome || !assunto){
+        document.getElementById('nome').style.border = "2px solid red"
+        document.getElementById('assunto').style.border = "2px solid red"
+        return
+    }
 
-if (!nome || !assunto){
-    document.getElementById('nome').style.border= '2px solid red'
-    document.getElementById('assunto').style.border = '2px solid red'
-    return
-}
-const mensagem = `gostaria de entrar em contato? `
-
-
-
+    //mandando msg por whats
+    const mensagem = `Gostaria de entrar em contato!!! \n\nNome: ${nome} \nAssunto: ${assunto}`
+    const msg = encodeURIComponent(mensagem)
+    window.location.href = `https://wa.me/5541991638186?text=${msg}`
 }
